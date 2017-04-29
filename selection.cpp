ab81732b42757845710dafb8da40e70007468636
sherin thomas <sherin.thomas@cowrks.com>
2017-04-29 18:46:04 +0530

    tabs to spaces

--- selection.cpp
+++ selection.cpp
@@ -3,32 +3,32 @@ using namespace std;
 
 void display(int *arr, int size)
 {
-	for (int i = 0; i < size; i++)
-	{
-		cout << arr[i] << endl;
-	}
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
 }
 
 
 int main()
 {
-	int size = 7;
-	int arrayin [size] = {1, 3, 9, 5, 2, 0, 6};
-	int temp;
-	for (int i = 0; i < size; i++)
-	{
-		int min = i;
-		for (int j = i + 1; j < size; j++)
-		{
-			if (arrayin[j] < arrayin[min])
-			{
-				min = j;
-			}
-		}
-		// Swap
-		temp = arrayin[i];
-		arrayin[i] = arrayin[min];
-		arrayin[min] = temp;
-	}
-	display(arrayin, size);
+    int size = 7;
+    int arrayin [size] = {1, 3, 9, 5, 2, 0, 6};
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arrayin[j] < arrayin[min])
+            {
+                min = j;
+            }
+        }
+        // Swap
+        temp = arrayin[i];
+        arrayin[i] = arrayin[min];
+        arrayin[min] = temp;
+    }
+    display(arrayin, size);
 }
